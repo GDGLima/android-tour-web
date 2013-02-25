80ad6db027bc418e17ae52dbcfd0965c7f91fae6
emedinaa <emedinaa@gmail.com>
2013-02-25 01:08:53 -0500

    navigation

--- js/app.js
+++ js/app.js
@@ -17,78 +17,170 @@
     var circle1;
     var circle2;
     var circle3;
+    var content;
+    var canvas;
+
 $(document).ready(function()
 {
     console.log("NO SEAN SAPOS" , "HOLA PIÑAN");
     console.log("ready ...",window.innerWidth);
-   // JQUERY4U.WIDGETS.TWITTER.init();
-    var canvas = document.getElementById("canvas");
-    canvas.style.width=window.innerWidth+'px';
-    stage=new createjs.Stage(canvas);
-    createjs.Touch.enable(stage);
-    stage.enableMouseOver();
+    $('#pagecontent').hide();
+    canvas=document.getElementById("canvas");
+    content=document.getElementById("pagecontent");
 
-    //draw circle
-    circle1=new Circle("GDGLima","#0FF");
-    circle2=new Circle("GDGArequipa","#0FF");
-    circle3=new Circle("AndroidTour","#0FF");
-    stage.addChild(circle1);
-    stage.addChild(circle2);
-    stage.addChild(circle3);
-
-    circle1.x=POSXCIRCLE;
-    circle1.y=100;
-
-    circle2.x=POSXCIRCLE+150;
-    circle2.y=80;
-
-    circle3.x=POSXCIRCLE+120;
-    circle3.y=220;
-
-    circle1.scaleX=0;
-    circle1.scaleY=0;
-
-    circle2.scaleX=0;
-    circle2.scaleY=0;
-
-    circle3.scaleX=0;
-    circle3.scaleY=0;
-
-    circle1.aux=1;
-    circle2.aux=0.8;
-    circle3.aux=0.9;
-
-    TweenLite.to(circle1, 0.6, {alpha:1,scaleX:1,scaleY:1,delay:1});
-    TweenLite.to(circle2, 0.6, {alpha:1,scaleX:0.8,scaleY:0.8,delay:1.3});
-    TweenLite.to(circle3, 0.6, {alpha:1,scaleX:0.9,scaleY:0.9,delay:1.6});
-    //events
-
-    circle1.addEventListener("mouseover", handleMouseOver);
-    circle1.addEventListener("mouseout", handleMouseOut);
-    circle2.addEventListener("mouseover", handleMouseOver);
-    circle2.addEventListener("mouseout", handleMouseOut);
-    circle3.addEventListener("mouseover", handleMouseOver);
-    circle3.addEventListener("mouseout", handleMouseOut);
-
-   // stage.addChild(btn1);
-    JQUERY4U.WIDGETS.TWITTER.setAux(showData);
-    JQUERY4U.WIDGETS.TWITTER.init();
-    createjs.Ticker.addEventListener("tick", stage);
-    $('#refresh-tweets').live('click', function(e)
-    {
-        reset();
-        e.preventDefault();
-        JQUERY4U.WIDGETS.TWITTER.setAux(showData);
-        JQUERY4U.WIDGETS.TWITTER.init();
-    });
-    createjs.Ticker.setInterval(30);
-    createjs.Ticker.setFPS(40);
-
-    //Subscribe to the Tick class. This will call the tick
-    //method at a set interval (similar to ENTER_FRAME with
-    createjs.Ticker.addEventListener("tick", tick);
+    //header
+        initHeader();
+    //container
+        initCanvas();
+    //footer
+        initFooter();
 });
 
+//header -------------------------------------------
+function goHome(e)
+{
+    $("#pagecontent").fadeOut(1000);
+    $("#canvas").fadeIn(1200);
+   // $('#canvas').show();
+    //$('#pagecontent').hide();
+}
+function navigationHandler(e)
+{
+    console.log("click ",e,e.target,e.target.id);
+   // $('#canvas').hide();
+   // $('#pagecontent').show();
+    var url="#";
+    switch(e.target.id)
+    {
+        case 'btnHome':
+                console.log("click Home");
+                url="assets/template/androidtour.html";
+            break;
+        case 'btnProgram':
+                console.log("click Program");
+            url="assets/template/program.html";
+            break;
+        case 'btnSpeaker':
+                console.log("click Speaker");
+            url="assets/template/speaker.html";
+            break;
+        case 'btnSponsor':
+                console.log("click Sponsor");
+            url="assets/template/sponsor.html";
+            break;
+        case 'btnTeam':
+                console.log("click Team");
+            url="assets/template/team.html";
+            break;
+    }
+   // $('#pagecontent').load(url);
+    hideCanvas(url);
+}
+function initHeader()
+{
+    $('#btnHome').bind("click",navigationHandler);
+    $('#btnProgram').bind("click",navigationHandler);
+    $('#btnSpeaker').bind("click",navigationHandler);
+    $('#btnSponsor').bind("click",navigationHandler);
+    $('#btnTeam').bind("click",navigationHandler);
+    $('#home').bind("click",goHome);
+}
+//container -------------------------------------------
+function initCanvas()
+{
+    // JQUERY4U.WIDGETS.TWITTER.init();
+       // canvas= document.getElementById("canvas");
+        canvas.style.width=window.innerWidth+'px';
+        stage=new createjs.Stage(canvas);
+        createjs.Touch.enable(stage);
+        stage.enableMouseOver();
+
+        //draw circle
+        circle1=new Circle("GDGLima","#0FF");
+        circle2=new Circle("GDGArequipa","#0FF");
+        circle3=new Circle("AndroidTour","#0FF");
+        stage.addChild(circle1);
+        stage.addChild(circle2);
+        stage.addChild(circle3);
+
+        circle1.x=POSXCIRCLE;
+        circle1.y=100;
+
+        circle2.x=POSXCIRCLE+150;
+        circle2.y=80;
+
+        circle3.x=POSXCIRCLE+120;
+        circle3.y=220;
+
+        circle1.scaleX=0;
+        circle1.scaleY=0;
+
+        circle2.scaleX=0;
+        circle2.scaleY=0;
+
+        circle3.scaleX=0;
+        circle3.scaleY=0;
+
+        circle1.aux=1;
+        circle2.aux=0.8;
+        circle3.aux=0.9;
+
+        TweenLite.to(circle1, 0.6, {alpha:1,scaleX:1,scaleY:1,delay:1});
+        TweenLite.to(circle2, 0.6, {alpha:1,scaleX:0.8,scaleY:0.8,delay:1.3});
+        TweenLite.to(circle3, 0.6, {alpha:1,scaleX:0.9,scaleY:0.9,delay:1.6});
+        //events
+
+        circle1.addEventListener("mouseover", handleMouseOver);
+        circle1.addEventListener("mouseout", handleMouseOut);
+        circle2.addEventListener("mouseover", handleMouseOver);
+        circle2.addEventListener("mouseout", handleMouseOut);
+        circle3.addEventListener("mouseover", handleMouseOver);
+        circle3.addEventListener("mouseout", handleMouseOut);
+
+       // stage.addChild(btn1);
+        JQUERY4U.WIDGETS.TWITTER.setAux(showData);
+        JQUERY4U.WIDGETS.TWITTER.init();
+        createjs.Ticker.addEventListener("tick", stage);
+        $('#refresh-tweets').live('click', function(e)
+        {
+            reset();
+            e.preventDefault();
+            JQUERY4U.WIDGETS.TWITTER.setAux(showData);
+            JQUERY4U.WIDGETS.TWITTER.init();
+        });
+        createjs.Ticker.setInterval(30);
+        createjs.Ticker.setFPS(40);
+
+        //Subscribe to the Tick class. This will call the tick
+        //method at a set interval (similar to ENTER_FRAME with
+        createjs.Ticker.addEventListener("tick", tick);
+}
+//footer  -------------------------------------------
+function initFooter()
+{
+
+}
+//canvas ----------------------------------------------
+function changePage(url)
+{
+    console.log("change page ");
+    //$('#canvas').hide();
+    //$('#pagecontent').show();
+    $('#pagecontent').load(url);
+    $('#pagecontent').hide();
+    $("#pagecontent").fadeIn(1500);
+   // content.alpha=0;
+   // TweenLite.to(content,0.8,{alpha:1});
+
+}
+function hideCanvas(url)
+{
+    $("#canvas").fadeOut(1200);
+    //$("#pagecontent").fadeOut(500);
+    changePage(url);
+  //TweenLite.to(canvas,0.8,{css:{autoAlpha:0}});
+  // TweenLite.to(canvas,0.8,{css:{alpha:0},onComplete:changePage,onCompleteParams:[url]})
+}
 function tick()
 {
     var date=new Date();
@@ -202,4 +294,5 @@ function handleImageLoad(event)
         }
         stage.update();
     }
+
 }
