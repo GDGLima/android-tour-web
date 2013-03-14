7f58bc3c7650a4e5ec31c3277b28c58a70c0e40e
Carlos Piñan <carlos.pinan@gmail.com>
2013-03-14 00:52:39 -0500

    Navigation JS added and separated from index.html

---
+++ js/navigation.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function()
+{
+  var nav_links =
+  {
+    "#android-tour" : "nav_androidtour.html",
+    "#programa" : "nav_programa.html",
+    "#speaker" : "nav_speaker.html",
+    "#sponsor" : "nav_sponsor.html",
+    "#team" : "nav_team.html"
+  };
+
+  function loadURL(key)
+  {
+    if(key.length > 0 && nav_links[key] != undefined)
+    {
+      jQuery("#page-container").html("<img src='assets/img/loader.gif'>")
+      jQuery("#page-container").load(nav_links[key]);
+    }
+  }
+
+  jQuery("ul.nav li a").click(function()
+  {
+    var key = jQuery(this).attr("href");
+    loadURL(key);
+  });
+
+  var currentURL = String(document.location).split("#");
+  var key = jQuery.trim(currentURL[currentURL.length -1]);
+
+  if(key.length > 0)
+    key = "#"+key;
+
+  
+  if(currentURL.length > 1)
+    loadURL(key);
+
+});
