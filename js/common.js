3b829bd1a39b22ef0e96e7b635bed173ba47f854
emedinaa <emedinaa@gmail.com>
2013-03-21 02:00:33 -0500

    common.js

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+	console.log("div android ",$("#divandroid"));
+    
+    if($("#divandroid"))
+    {
+        $("#divandroid").hide();
+        $("#divandroid").fadeIn("slow");
+    }
+})
--- js/navigation.js
+++ js/navigation.js
@@ -21,7 +21,7 @@ jQuery(document).ready(function()
   jQuery("ul.nav li a").click(function()
   {
     var key = jQuery(this).attr("href");
-    console.log("navigation ","key ",key);
+    console.log("navigation ","key "+key);
     loadURL(key);
   });
 
